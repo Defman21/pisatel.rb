2cb60a19e4df924a9f04710dc612ef5642ea7aaa
Defman21 <i@defman.me>
2017-08-14 21:31:23 +0300

    Use db/config.yaml for the db connection; fixed #1
    Signed-off-by: Defman21 <i@defman.me>

--- db/database.rb
+++ db/database.rb
@@ -1,5 +1,5 @@
 module DB
-  Connection = Sequel.sqlite('./db/database.db')
+  Connection = Sequel.connect YAML.load(File.read './db/config.yaml')[ENV['RACK_ENV']]
   Connection.sql_log_level = :debug
   Connection.logger = ::Logging::Logger
 end
