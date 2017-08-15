1342da8b7a6a8f15b6129f781703e8c78b2d5e76
Defman21 <i@defman.me>
2017-08-15 17:42:49 +0300

    Puma: removed socket binding
    Signed-off-by: Defman21 <i@defman.me>

--- puma.rb
+++ puma.rb
@@ -5,11 +5,7 @@ root = Dir.pwd
 threads 1, 4
 workers 3
 
-if !ENV['USE_SOCKET'].nil? && ENV['USE_SOCKET'] == 1
-  bind "unix://#{File.join(root, 'tmp/sock/puma.sock')}"
-else
-  bind "tcp://0.0.0.0:8080"
-end
+bind "tcp://0.0.0.0:8080"
 pidfile File.join(root, 'tmp/pid/puma.pid')
 
 environment ENV['RACK_ENV'] || 'production'
