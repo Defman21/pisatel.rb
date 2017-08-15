7deb5cbe15fd4624deda25ffa068e46672f16468
Defman21 <i@defman.me>
2017-08-15 17:44:35 +0300

    Docker: entrypoint does not handle the prod/dev mode anymore, it's the job of the Rakefile
    Signed-off-by: Defman21 <i@defman.me>

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,4 +3,4 @@ echo "Running puma"
 cd /usr/src/app && \
 bundle exec sequel -m db/migrate db/config.yaml -E -e $RACK_ENV && \
 rake assets:recompile && \
-if [ "$RACK_ENV" == "production" ]; then bundle exec puma -C ./puma.rb; else rake server:run; fi
+rake server:run
